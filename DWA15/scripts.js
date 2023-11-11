d15aec8c1657eaff0c425a303c0be28122a4cb5e
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-11-11 13:48:27 +0200

    Completed lesson 12 on Scrimba

--- DWA15/scripts.js
+++ DWA15/scripts.js
@@ -1,14 +1,42 @@
-function MyMessage() {
-    return (
-        <div>
-            <h2> Hello World!</h2>;
-        </div>
-    );
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-    <div>
-        <MyMessage/>
-    </div>,
-    document.getElementById("root")
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(navbar);
+
+// const navbar = (
+
+//         <nav>
+//            <h1> Website</h1>
+//             <ul>
+//                 <li>Pricing</li>
+//                 <li>About</li>
+//                 <li>Contact</li>
+//             </ul>
+//         </nav>
+// )
+// ReactDOM.render(navbar, document.getElementById("root"))
+
+// const navbar = (
+//     <nav>
+//         <h1>Bob's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// );
+
+// ReactDOM.render(navbar, document.getElementById("root"));
