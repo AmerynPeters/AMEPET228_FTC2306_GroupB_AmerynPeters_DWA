197268321abd565c68da24a1226ea49b1417ca0d
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-11-11 14:14:05 +0200

    Completed lesson 13

--- DWA15/scripts.js
+++ DWA15/scripts.js
@@ -1,19 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(navbar);
+document.getElementById("root").append(page);
+// const navbar = (
+//     <nav>
+//         <h1>Bob's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// );
+// document.getElementById("root").append(navbar)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(document.append (navbar));
 
 // const navbar = (
 
