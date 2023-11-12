bfbd560d7d54006d946b830f003d09b3941953fa
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-11-12 20:53:07 +0200

    completed lesson 19

--- DWA15/scripts.js
+++ DWA15/scripts.js
@@ -1,31 +1,22 @@
-// const page = (
-//     <div>
-//         <img src="./react-logo.png" width="40px" />
-//         <h1>Fun facts about React</h1>
-//         <ul>
-//             <li>Was first released in 2013</li>
-//             <li>Was originally created by Jordan Walke</li>
-//             <li>Has well over 100K stars on GitHub</li>
-//             <li>Is maintained by Facebook</li>
-//             <li>Powers thousands of enterprise apps, including mobile apps</li>
-//         </ul>
-//     </div>
-// );
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    );
+}
 
-// ReactDOM.render(page, document.getElementById("root"));
-// function Head() {
-//     return (
+function Foot() {
+   return (<footer>
+        <small> © 2023 Peters development. All rights reserved.</small>
+    </footer> ) 
+}
 
-//     );
-
-function Page() {
+function Content() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px" />
-                </nav>
-            </header>
             <h1>Why I am excited to learn React!</h1>
             <ol>
                 <li>
@@ -37,9 +28,15 @@ function Page() {
                     my code
                 </li>
             </ol>
-            <footer>
-                <small>© 2023 Peters development. All rights reserved.</small>
-            </footer>
+        </div>
+    );
+}
+function Page() {
+    return (
+        <div>
+            <Header />
+            <Content/>
+            <Foot />
         </div>
     );
 }
