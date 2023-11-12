929fbd36d36a00e018941d191106796a152d058f
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-11-12 10:28:59 +0200

    completed lesson 16 'custom components'

--- DWA15/scripts.js
+++ DWA15/scripts.js
@@ -1,43 +1,37 @@
-const page = (
-    <div>
-        <img src="./react-logo.png" width="40px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-);
-
-ReactDOM.render(page, document.getElementById("root"));
-/*
- document.getElementById("root").append(navbar)
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(document.append (navbar));
-
-// const navbar = (
-
-//         <nav>
-//            <h1> Website</h1>
-//             <ul>
-//                 <li>Pricing</li>
-//                 <li>About</li>
-//                 <li>Contact</li>
-//             </ul>
-//         </nav>
-// )
-// ReactDOM.render(navbar, document.getElementById("root"))
-
-// const navbar = (
-//     <nav>
-//         <h1>Bob's Bistro</h1>
+// const page = (
+//     <div>
+//         <img src="./react-logo.png" width="40px" />
+//         <h1>Fun facts about React</h1>
 //         <ul>
-//             <li>Menu</li>
-//             <li>About</li>
-//             <li>Contact</li>
+//             <li>Was first released in 2013</li>
+//             <li>Was originally created by Jordan Walke</li>
+//             <li>Has well over 100K stars on GitHub</li>
+//             <li>Is maintained by Facebook</li>
+//             <li>Powers thousands of enterprise apps, including mobile apps</li>
 //         </ul>
-//     </nav>
-*/
+//     </div>
+// );
+
+// ReactDOM.render(page, document.getElementById("root"));
+
+function Page() {
+    
+        return (
+            <div>
+                <h1>Why I am excited to learn React!</h1>
+                <ol>
+                    <li>
+                        It is going to make my life so much easier when I've
+                        learnt to use it properly
+                    </li>
+                    <li>
+                        It will save me a lot of time that I might spend
+                        debugging my code
+                    </li>
+                </ol>
+            </div>
+        );
+    
+};
+
+ReactDOM.render(<Page/>, document.getElementById("root"))
